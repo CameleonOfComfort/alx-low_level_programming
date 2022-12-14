3fae71417c7ec7fb87794043193b72d5f815c996
CameleonOfComfort <erickowhite@hotmail.com>
2022-12-14 07:21:55 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,12 +2,12 @@
 
 /**
  * print_sign - Prints the sign of a number.
- * @n: The number of which the sign will be printed.
- *
- * Return: 1 if the number is greater than zero,
- *             0 if the number is zero,
- *             -1 if the number is less than zero.
- **/
+ * @n: the num to checking for
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -21,9 +21,4 @@ int print_sign(int n)
               	return (0);
 	}
 	else
-	{
-		_putchar('-');
-		return (-1);
 	}
-
-}
