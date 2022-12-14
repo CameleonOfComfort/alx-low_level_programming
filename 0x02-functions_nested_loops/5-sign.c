fabf6afc6b18ec775e2f7389de9622206855f53c
CameleonOfComfort <erickowhite@hotmail.com>
2022-12-14 09:46:39 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * print_sign - return 0 letter not lowercase, 1 letter lowercase
- *
- * @n : number to check
- * Return: 0 or 1
+ * print_sign - Prints the sign of a number.
+ * @n: The number of which the sign will be printed.
+ * Return: 1 if the number is greater than zero,
+ * 0 if the number is zero,
+ * -1 if the number is less than zero.
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		test = 1;
 		_putchar('+');
+		return (1);
 	}
-	else if (n == 0)
-	{
-		test = 0;
-		_putchar('0');
-	}
+			else if (n == 0)
+			{
+				_putchar('0');
+				return (0);
+			}
 	else
 	{
-		test = -1;
 		_putchar('-');
+		return (-1);
 	}
-	return (test);
 }
-
