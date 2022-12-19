3afccc0bd5723fbdf4668e65a0ccdb664abe33b4
CameleonOfComfort <erickowhite@hotmail.com>
2022-12-19 12:47:15 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -17,11 +17,12 @@ void rev_string(char *s)
 	while (s[a] != '\0')
 	{
 		a++;
-		c = a - 1;
-		for (b = 0; c >= 0 && b < c; c--, b++)
-		{
-			d = s[b];
-			s[b] = s[c];
-			s[c] = d;
-		}
+	}
+	c = a - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
+	}
 }
